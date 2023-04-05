7643f6f8cf5e03cedd3193ad4b527a45f50af619
mitchcamza <192mitch88@gmail.com>
2023-04-05 15:55:26 +0200

    Add class containing constructor and destructor for raw pointer

--- ch02-pointer-destructor/main.cpp
+++ ch02-pointer-destructor/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
-using namespace std;
+class MyClass
+{
+private:
+    int* data_;
+
+public:
+    MyClass()
+    {
+        data_ = new int[5];
+        std::cout << "Constructor called" << std::endl;
+    }
+
+    ~MyClass()
+    {
+        delete[] data_;
+        std::cout << "Destructor called" << std::endl;
+    }
+};
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    MyClass obj;
     return 0;
 }
